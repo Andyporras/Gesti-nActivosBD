c3773862e06001f43e4d657e0441477c5ed53a32
Andy Porras <andyporras@estudiantec.cr>
2022-09-02 15:43:56 -0600

    creación del proyecto

---
+++ Programa/GestiónActivos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestiónActivos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
